e37e75a423a5eb24a7ccc22a5c0e0401c8cb0121
Victor de la Rosa <victorwmh18@gmail.com>
2020-03-29 23:00:44 -0400

    Added optional format download

--- YoutubeDownloader/Controllers/DownloadController.cs
+++ YoutubeDownloader/Controllers/DownloadController.cs
@@ -5,9 +5,10 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;   
 using YoutubeExplode;
 using YoutubeExplode.Models.MediaStreams;
+using YoutubeAPI.Enums;
 
 namespace YoutubeDownloader.Controllers
 {
@@ -17,21 +18,41 @@ namespace YoutubeDownloader.Controllers
     {
 
         [HttpGet("{url}")]
-        public async Task<IActionResult> Download(string url, string format)
+        public async Task<IActionResult> Download(string url, FormatoDescarga format, string TiempoInicio, string TiempoFin)
         {
-            var client = new YoutubeClient();
-            var id = YoutubeClient.ParseVideoId(WebUtility.UrlDecode(url));
-            var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id);
-            var video = await client.GetVideoAsync(id);
+            try
+            {
+                var client = new YoutubeClient();
+                var id = YoutubeClient.ParseVideoId(WebUtility.UrlDecode(url));
+                var streamInfoSet = await client.GetVideoMediaStreamInfosAsync(id);
+                var video = await client.GetVideoAsync(id);
+                var tempStream = new MemoryStream();
+                MediaStreamInfo streamInfo;
 
-            MediaStreamInfo streamInfo = streamInfoSet.Audio.WithHighestBitrate();
-            var tempStream = new MemoryStream();
+                switch (format)
+                {
+                    case FormatoDescarga.mp4:
+                        streamInfo = streamInfoSet.Muxed.WithHighestVideoQuality();
+                        break;
+                    case FormatoDescarga.mp3:
+                        streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+                        break;
+                    default:
+                        format = FormatoDescarga.mp3;
+                        streamInfo = streamInfoSet.Audio.WithHighestBitrate();
+                        break;
+                }
+                await client.DownloadMediaStreamAsync(streamInfo, tempStream);
+                tempStream.Seek(0, SeekOrigin.Begin);
+                var ext = streamInfo.Container.GetFileExtension();
 
-            await client.DownloadMediaStreamAsync(streamInfo, tempStream);
-            tempStream.Seek(0, SeekOrigin.Begin);
-            var ext = streamInfo.Container.GetFileExtension();
-
-            return new FileStreamResult(tempStream, "application/octet-stream") { FileDownloadName = $"descarga.mp3"};
+                return new FileStreamResult(tempStream, "application/octet-stream") { FileDownloadName = $"{video.Title}.{format}" };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return NotFound();
+            }  
         }
 
     }
---
+++ YoutubeDownloader/Enums/FormatoDescarga.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoutubeAPI.Enums
+{
+    public enum FormatoDescarga
+    {
+        mp4 = 1,
+        mp3 = 2
+    }
+}
